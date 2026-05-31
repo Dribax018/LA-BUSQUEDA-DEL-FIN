86e64d0e9503347a792d9467e5b5174061a0d9e5
delgadomarcos391-collab <delgadomarcos391@gmail.com>
2026-05-31 14:04:33 +0200

    he cambiado la escala de krystoforo y he cambiado el script de cambio de escena para que te envíe a la pelea final.

--- Assets/FILES INDIVIDUALES/ALEJANDRO_FILE/SCRIPS/CambiarEscenaTrigger.cs
+++ Assets/FILES INDIVIDUALES/ALEJANDRO_FILE/SCRIPS/CambiarEscenaTrigger.cs
@@ -2,18 +2,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class CambiarEscenaTrigger : MonoBehaviour
 {
+    [Header("Cambio de escena")]
+    [SerializeField] private string pelea;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(pelea);
         }
-        if (collision.gameObject.tag == "")
+        if (collision.gameObject.tag == "Untagged")
         {
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(pelea);
         }
     }
 }
